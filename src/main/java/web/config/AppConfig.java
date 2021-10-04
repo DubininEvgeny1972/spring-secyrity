3bddff80cd7d752ce0c0c0ef6537dedfac6d9654
Евгений <87427438+DubininEvgeny1972@users.noreply.github.com>
2021-10-04 15:52:32 +0300

    Проба2

--- src/main/java/web/config/AppConfig.java
+++ src/main/java/web/config/AppConfig.java
@@ -1,20 +1,22 @@
 package web.config;
 
 import com.mchange.v2.c3p0.ComboPooledDataSource;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import org.hibernate.jpa.HibernatePersistenceProvider;
 import org.springframework.context.annotation.*;
 import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import web.model.User;
+
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 import java.beans.PropertyVetoException;
-import java.util.Objects;
 import java.util.Properties;
 
 
@@ -23,28 +25,8 @@ import java.util.Properties;
 @EnableTransactionManagement
 @ComponentScan("web")
 @PropertySource("classpath:application.properties")
+
 public class AppConfig {
-
-    @Bean
-    public JpaTransactionManager transactionManager() throws PropertyVetoException {
-        JpaTransactionManager transactionManager =
-                new JpaTransactionManager(Objects.requireNonNull(entityManagerFactory().getObject()));
-        transactionManager.setDataSource(dataSource());
-
-        return transactionManager;
-    }
-
-
-    @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory() throws PropertyVetoException {
-        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean(); // HibernateExceptions, PersistenceExceptions... to DataAccessException
-        em.setDataSource(dataSource());
-        em.setPackagesToScan("web.model");
-        em.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
-        return em;
-    }
-
-
     private Environment env;
 
     public AppConfig(Environment env) {
@@ -58,18 +40,42 @@ public class AppConfig {
         dataSource.setJdbcUrl(env.getProperty("db.url"));
         dataSource.setUser(env.getProperty("db.username"));
         dataSource.setPassword(env.getProperty("db.password"));
-        Properties properties = new Properties();
-        properties.put("hibernate.hbm2ddl.auto = create-drop", env.getRequiredProperty("hibernate.hbm2ddl.auto = create-drop"));
-        dataSource.setProperties(properties);
         return dataSource;
     }
 
-
 //    @Bean
-//    public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
-//        JpaTransactionManager transactionManager = new JpaTransactionManager();
-//        transactionManager.setEntityManagerFactory(emf);
-//        return transactionManager;
+//    public LocalContainerEntityManagerFactoryBean entityManagerFactory() throws PropertyVetoException {
+//        JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+//        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean(); // HibernateExceptions, PersistenceExceptions... to DataAccessException
+//        em.setDataSource(dataSource());
+//        em.setPackagesToScan("web.model");
+//        em.setJpaVendorAdapter(vendorAdapter);
+//        return em;
 //    }
 
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean getEntityManagerFactoryBean()  throws PropertyVetoException {
+        JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        LocalContainerEntityManagerFactoryBean entityManagerFactory = new LocalContainerEntityManagerFactoryBean(); // HibernateExceptions, PersistenceExceptions... to DataAccessException
+
+        entityManagerFactory.setDataSource(dataSource());
+        entityManagerFactory.setPackagesToScan("web.model");
+//        entityManagerFactory.setJpaVendorAdapter(vendorAdapter);
+        entityManagerFactory.setPersistenceProviderClass(HibernatePersistenceProvider.class);
+        entityManagerFactory.setJpaVendorAdapter(vendorAdapter);
+
+        Properties props = new Properties();
+        entityManagerFactory.setJpaProperties(props);
+        props.put("hibernate.show_sql", env.getRequiredProperty("hibernate.show_sql"));
+        props.put("hibernate.hbm2ddl.auto", env.getRequiredProperty("hibernate.hbm2ddl.auto"));
+        return entityManagerFactory;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(emf);
+        return transactionManager;
+    }
 }
--- src/main/java/web/controller/LoginController.java
+++ src/main/java/web/controller/LoginController.java
@@ -28,30 +28,30 @@ public class LoginController {
         return "login";
     }
 
-    @PostConstruct
-    public void startProject() {
-        Role role1 = new Role("ADMIN");
-        Role role2 = new Role("USER");
-        User user1 = new User();
-        user1.setName("Jon");
-        user1.setLastName("Lennon");
-        user1.setAge((byte) 35);
-        user1.setUserName("111");
-        user1.setPassword("111");
-        Set<Role> roleFromUser1 = new HashSet<>();
-        roleFromUser1.add(role1);
-        user1.setRoles(roleFromUser1);
-        service.saveUser(user1);
-        User user2 = new User();
-        user2.setName("Kirk");
-        user2.setLastName("Duglas");
-        user2.setAge((byte) 45);
-        user2.setUserName("222");
-        user2.setPassword("222");
-        Set<Role> roleFromUser2 = new HashSet<>();
-        roleFromUser1.add(role1);
-        roleFromUser2.add(role2);
-        user2.setRoles(roleFromUser1);
-        service.saveUser(user2);
-    }
+//    @PostConstruct
+//    public void startProject() {
+//        Role role1 = new Role("ADMIN");
+//        Role role2 = new Role("USER");
+//        User user1 = new User();
+//        user1.setName("Jon");
+//        user1.setLastName("Lennon");
+//        user1.setAge((byte) 35);
+//        user1.setUserName("111");
+//        user1.setPassword("111");
+//        Set<Role> roleFromUser1 = new HashSet<>();
+//        roleFromUser1.add(role1);
+//        user1.setRoles(roleFromUser1);
+//        service.saveUser(user1);
+//        User user2 = new User();
+//        user2.setName("Kirk");
+//        user2.setLastName("Duglas");
+//        user2.setAge((byte) 45);
+//        user2.setUserName("222");
+//        user2.setPassword("222");
+//        Set<Role> roleFromUser2 = new HashSet<>();
+//        roleFromUser1.add(role1);
+//        roleFromUser2.add(role2);
+//        user2.setRoles(roleFromUser1);
+//        service.saveUser(user2);
+//    }
 }
